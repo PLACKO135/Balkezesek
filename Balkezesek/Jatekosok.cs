e20a096bcea14d9f31ff24eab388566557aba905
P.Laszlo <70635967+PLACKO135@users.noreply.github.com>
2023-11-14 15:52:49 +0100

    Add files via upload

---
+++ Balkezesek/Jatekosok.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Balkezesek
+{
+    internal class Jatekosok
+{
+        string nev;
+        DateTime elso;
+        DateTime utolso;
+        int suly;
+        int magassag;
+
+        public string Nev { get => nev; set => nev = value; }
+        public DateTime Elso { get => elso; set => elso = value; }
+        public DateTime Utolso { get => utolso; set => utolso = value; }
+        public int Suly { get => suly; set => suly = value; }
+        public int Magassag { get => magassag; set => magassag = value; }
+
+        public Jatekosok(string sor)
+        {
+            string[] adatok = sor.Trim().Split(';');
+            Nev = adatok[0];
+            Elso = DateTime.Parse(adatok[1]);
+            Utolso = DateTime.Parse(adatok[2]);
+            Suly = int.Parse(adatok[3]);
+            Magassag = int.Parse(adatok[4]);
+        }
+    }
+
+}
--- Balkezesek/Program.cs
+++ Balkezesek/Program.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using Balkezesek;
+using System.Numerics;
+using System.Text;
+
+
+List<Jatekosok> lista = new List<Jatekosok>();
+
+
+StreamReader sr = new StreamReader("balkezesek.csv");
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    lista.Add(new Jatekosok(sr.ReadLine()));
+}
+sr.Close();
+
+//3.
+Console.WriteLine($"3. feladat: {lista.Count}");
+
+//4. 
+Console.WriteLine($"4. feladat:");
+
+lista.Where(x => x.Utolso.ToString("yyyy-MM").Contains("1999-10")).ToList().ForEach(x => Console.WriteLine($"\t{x.Nev} {Math.Round(x.Magassag * 2.54, 1)} cm"));
+//5.
+Console.WriteLine($"5. feladat:");
+
+bool bennevan = false;
+int kapottszam = 0;
+
+while (bennevan == false)
+{
+    kapottszam = Convert.ToInt32(Console.ReadLine());
+    if (kapottszam >= 1990 && kapottszam <= 1999)
+    {
+        bennevan = true;
+        Console.WriteLine("Ügyes vagy:)");
+    }
+    else
+    {
+        Console.Write("Hibás adat kérek egy 1990 és 1999 közötti évszámot!:");
+    }
+}
+//6. 
+double OsszSuly = 0;
+double Darab = 0;
+for (int i = 0; i < lista.Count; i++)
+{
+    if (kapottszam >= lista[i].Elso.Year && kapottszam <= lista[i].Utolso.Year)
+    {
+        OsszSuly += lista[i].Suly;
+        Darab++;
+    }
+}
+double atlagsuly = OsszSuly / Darab;
+Console.WriteLine($"6. feladat:{Math.Round(atlagsuly, 2)} font ");
